42f495c53e0b311729e460d82a4571559278c204
piyush mevada <piyushmevada1606@gmail.com>
2025-02-04 01:47:46 +0530

    add validation client and serverside

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const Listing = require("./models/listing.js"); 
+const Listing = require("./models/listing.js");
 const app = express();
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 const methodOverride = require('method-override');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-const ejsMate=require("ejs-mate");
+const ejsMate = require("ejs-mate");
 app.engine("ejs", ejsMate);
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wonderlust");
@@ -18,6 +21,15 @@ main().catch(err => console.log(err));
 
 let port = 8080;
 
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        const err=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }else{
+        next();
+    }
+}
 
 app.get("/", (req, res) => {
     console.log("Hello, Home Route");
@@ -26,50 +38,59 @@ app.get("/", (req, res) => {
 
 
 //index route
-app.get("/listing",async(req,res)=>{
-    const AllListing=await Listing.find({}); 
-    console.log(AllListing);
-    res.render("listings/index.ejs",{AllListing});
-});
+app.get("/listing", wrapAsync(async (req, res) => {
+        const AllListing = await Listing.find({});
+        console.log(AllListing);
+        res.render("listings/index.ejs", { AllListing });
+}));
 
 //new route
-app.get("/listing/new",(req,res)=>{
+app.get("/listing/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
 //show route
-app.get("/listing/:id",async(req,res)=>{
-    let {id}=req.params;
-    const item=await Listing.findById(id);
-    res.render("listings/show.ejs",{item});
-});
+app.get("/listing/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const item = await Listing.findById(id);
+    res.render("listings/show.ejs", { item });
+}));
 //create route
-app.post("/listing",async(req,res)=>{
+app.post("/listing", validateListing,wrapAsync(async (req, res) => {
     // let {title,price,location,country,image}=req.body;
-    const newlisting=new Listing(req.body.listing);
+    const newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listing");
-});
+}));
 
-app.get("/listing/:id/edit",async(req,res)=>{
-    let {id}=req.params;
-    const item=await Listing.findById(id);
-    res.render("listings/edit.ejs",{item});
-});
+app.get("/listing/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const item = await Listing.findById(id);
+    res.render("listings/edit.ejs", { item });
+}));
 
 //update route
-app.put("/listing/:id",async(req,res)=>{
-    let {id}=req.params;
-    const item=await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put("/listing/:id",validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const item = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listing");
 
-});
+}));
 
 //delete route
-app.delete("/listing/:id",async(req,res)=>{
-    let {id}=req.params;
-    const deleteitem=await Listing.findByIdAndDelete(id);
+app.delete("/listing/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const deleteitem = await Listing.findByIdAndDelete(id);
     res.redirect("/listing");
+}));
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"page not found"));
+});
+
+app.use((err, req, res, next) => {
+    let {statusCode=500,message="Something want wrong!"}=err;
+    res.status(statusCode).render("listings/error.ejs",{message});
 });
 
 app.listen(port, () => {
